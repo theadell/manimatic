80cb5c8c3ae24aadd02cc18ac9de5a980e91b8bb
theadell <65723807+theadell@users.noreply.github.com>
2024-12-15 17:08:02 +0100

    create TLS certificate for frontend

--- infra/lib/cloudfront-tls-cert-stack.ts
+++ infra/lib/cloudfront-tls-cert-stack.ts
@@ -3,18 +3,18 @@ import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import { Construct } from 'constructs';
 
-export class CertStack extends cdk.Stack {
+export class CloudfrontTlsCertStack extends cdk.Stack {
   public readonly certificate: acm.Certificate;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-      domainName: 'aws.adelhub.com',
+      domainName: 'aws.adelh.dev',
     });
 
     this.certificate = new acm.Certificate(this, 'TLSCertificate', {
-      domainName: 'manimatic.aws.adelhub.com',
+      domainName: 'manimatic.aws.adelh.dev',
       validation: acm.CertificateValidation.fromDns(hostedZone),
     });
   }
