775949c0c62f66542c308d4fe90fb1b3e6bf4b92
theadell <65723807+theadell@users.noreply.github.com>
2024-12-24 19:20:20 +0100

    setup DLQ in local development

--- init-awslocal.sh
+++ init-awslocal.sh
@@ -5,11 +5,28 @@ export AWS_ACCESS_KEY_ID=000000000000
 export AWS_SECRET_ACCESS_KEY=000000000000
 export AWS_DEFAULT_REGION=${REGION:-us-east-1}
 
-# Create the task queue
-echo "Creating Task Queue: $TASK_QUEUE_NAME"
+DLQ_NAME="TaskQueueDLQ"
+echo "Creating Dead Letter Queue: $DLQ_NAME"
+
+DLQ_URL=$(awslocal sqs create-queue \
+  --queue-name "$DLQ_NAME" \
+  --region "$REGION" \
+  --query 'QueueUrl' \
+  --output text)
+
+DLQ_ARN=$(awslocal sqs get-queue-attributes \
+  --queue-url "$DLQ_URL" \
+  --attribute-names QueueArn \
+  --query 'Attributes.QueueArn' \
+  --output text)
+
+
+# Create the Task queue with DLQ configuration
+echo "Creating Task Queue: $TASK_QUEUE_NAME with DLQ and max receives = 2"
 awslocal sqs create-queue \
   --queue-name "$TASK_QUEUE_NAME" \
-  --region "$REGION"
+  --region "$REGION" \
+  --attributes "{\"RedrivePolicy\":\"{\\\"deadLetterTargetArn\\\":\\\"$DLQ_ARN\\\",\\\"maxReceiveCount\\\":\\\"2\\\"}\"}"
 
 # Create the result queue
 echo "Creating Result Queue: $RESULT_QUEUE_NAME"
